50f3490fe672b61ad0585ef31ecad73df6232cf0
mennaabdeltawab07-dotcom <mennaabdeltawab07@gmail.com>
2025-12-10 20:03:58 +0200

    up

--- src/CarGame/TheGame.java
+++ src/CarGame/TheGame.java
@@ -4,14 +4,16 @@ import javax.swing.*;
 import java.awt.*;
 
 public class TheGame extends JFrame {
-    private CardLayout  cardLayout;
+    private CardLayout cardLayout;
     private JPanel contentPanel;
     private JLayeredPane layeredPane;
     public String gameMode;
-//    main
+
+    //    main
     public static void main(String[] args) {
-        SwingUtilities.invokeLater( () -> new TheGame());
+        SwingUtilities.invokeLater(() -> new TheGame());
     }
+
     public TheGame() {
         cardLayout = new CardLayout();
         contentPanel = new JPanel(cardLayout);
@@ -21,44 +23,40 @@ public class TheGame extends JFrame {
 
 
 //
-        homePage.setButtonAction(e ->{
+        homePage.setButtonAction(e -> {
             String command = e.getActionCommand();
-            if("Exit".equals(command)){
-               dispose();
-            }
-            else if("signIn".equals(command)){
+            if ("Exit".equals(command)) {
+                dispose();
+            } else if ("signIn".equals(command)) {
                 homePage.showSignInDialog();
-            }
-            else if("howToPlay".equals(command)){
-                cardLayout.show(contentPanel,"howToPlay");
-            } 
-
+            } else if ("howToPlay".equals(command)) {
+                cardLayout.show(contentPanel, "howToPlay");
             }
 
 
         });
 
 
-
-        contentPanel.add(homePage, "HomePage"   );
+        contentPanel.add(homePage, "HomePage");
         contentPanel.add(howToPlay, "howToPlay");
 
 
 //        باقي الpages
         layeredPane = new JLayeredPane();
         layeredPane.setLayout(null);
-        layeredPane.setPreferredSize(new Dimension(700,500));
-        layeredPane.add(contentPanel,JLayeredPane.DEFAULT_LAYER);
-        contentPanel.setBounds(0,0,700,500);
+        layeredPane.setPreferredSize(new Dimension(700, 500));
+        layeredPane.add(contentPanel, JLayeredPane.DEFAULT_LAYER);
+        contentPanel.setBounds(0, 0, 700, 500);
 
         setTitle("Car Game");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(700,500);
+        setSize(700, 500);
         setLocationRelativeTo(null);
         getContentPane().add(layeredPane);
         setVisible(true);
 
 
     }
-
 }
+
+
